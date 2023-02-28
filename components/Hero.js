bdfc4205314ee021df9944f87bcdae83661be48c
EnergyBoyTW <sid850824@gmail.com>
2023-02-27 23:26:43 -0800

    at least a version

--- components/Hero.js
+++ components/Hero.js
@@ -1,14 +1,29 @@
 import React from "react";
 
 const Hero = ({data}) => {
+
+  console.log(data.languages)
+  var languages = data.languages
+
+  var language_str = ''
+  for (let i = 0; i < languages.length; i++) {
+    language_str+=languages[i].name
+  }
+  console.log(language_str)
   return (
     <div>
             <div>
-                <p>{data.name}</p>
+                <p>Name: {data.name}</p>
             </div>
             <div>
-                <p>{data.emoji}</p>
+                <p>Emoji: {data.emoji}</p>
             </div>
+
+            <div>
+                <p>Languages: {language_str}</p>
+            </div>
+            <br></br>
+
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,4 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Link from 'next/link'
-import Hero from '../components/Hero'
 import App from '../components/App'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -12,7 +7,6 @@ export default function Home() {
   return (
     <>
       <App />
-      <h1>Hello</h1>
     </>
   )
 }
